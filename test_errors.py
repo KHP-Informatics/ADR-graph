37aab64d2c131f2729f54c9edcb955f4bb6d0a63
Daniel Bean <daniel.bean@kcl.ac.uk>
2021-02-15 18:20:25 +0000

    print

--- test_errors.py
+++ test_errors.py
@@ -5,13 +5,13 @@ import EdgePrediction, json, time
 ep = EdgePrediction.EdgePrediction()
 
 #load data from edge list
-print("file has target node error")
+print("TEST file has target node error")
 try:
     ep.CSV_to_graph(fname = 'data/data_tgt_error.csv', encoding='utf-8-sig')
 except Exception as err:
     print(err)
 
-print("file has src node error")
+print("TEST file has src node error")
 ep = EdgePrediction.EdgePrediction()
 try:
     ep.CSV_to_graph(fname = 'data/data_src_error.csv', encoding='utf-8-sig')
@@ -21,7 +21,7 @@ except Exception as err:
 
 ep = EdgePrediction.EdgePrediction()
 ep.CSV_to_graph(fname = 'data/data_no_common_src.csv', encoding='utf-8-sig')
-print("file has no common source nodes")
+print("TEST file has no common source nodes")
 try:
     ep.preprocess()
 except Exception as err:
@@ -30,7 +30,7 @@ except Exception as err:
 #load a network with no common source nodes, catch the error but try to run anyway
 ep = EdgePrediction.EdgePrediction()
 ep.CSV_to_graph(fname = 'data/data_no_common_src.csv', encoding='utf-8-sig')
-print("file has no common source nodes")
+print("TEST load file with no common source nodes then try to predict")
 try:
     ep.preprocess()
 except Exception as err:
@@ -49,7 +49,7 @@ ep = EdgePrediction.EdgePrediction()
 ep.CSV_to_graph(fname = 'data/data.csv')
 ep.preprocess()
 target_name = "C0027849"
-print("did not set to_predict")
+print("TEST did not set to_predict")
 try:
     result = ep.predict(target = target_name)
 except Exception as err:
@@ -58,7 +58,7 @@ except Exception as err:
 #try to use an invalid optimisation method
 ep.to_predict = 'HAS_SIDE_EFFECT'
 ep.optimisation_method = "something not implemented"
-print("set an invalid optimisation method")
+print("TEST set an invalid optimisation method")
 try:
     result = ep.predict(target = target_name)
 except Exception as err:
